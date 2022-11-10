8c2362377eda8e3e376794eec1f0bc2106c0a015
B.R.O.L.Y <114589218+RIDWANE-EL-FILALI@users.noreply.github.com>
2022-11-10 12:12:25 -0800

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
 size_t	ft_strlen(const char *s)
 {
